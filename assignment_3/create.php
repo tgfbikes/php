6c326706bb51fbcaed33da5f93884b48d6064956
Stephen King <stephen@rocketmade.com>
2016-01-27 11:52:43 -0700

    initial config for hooking up to database

---
+++ assignment_3/create.php
@@ -0,0 +1,14 @@
+<?php include('./includes/header.php'); ?>
+
+<?php 
+  // get values from $_POST
+
+  // redirect if not POST
+
+  // connect to the database
+  $mysql_connection = mysqli_connect('mysql.cs.dixie.edu', 'sking', 'P@$$word', 'sking');
+
+  // insert into the table
+?>
+
+<?php include('./includes/footer.php'); ?>
