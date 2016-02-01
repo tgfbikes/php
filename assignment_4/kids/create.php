79c7b71ead31e0577c35df8f85c630da89277a47
Stephen King <stephen@rocketmade.com>
2016-02-01 11:39:32 -0700

    created new project based off of assignment_3

---
+++ assignment_4/kids/create.php
@@ -0,0 +1,72 @@
+<?php include('../includes/header.php'); ?>
+
+<?php 
+  if (isset($_POST['submit'])) {
+    // get values from $_POST
+    $first_name     = $_POST['first_name'];
+    $middle_name    = $_POST['middle_name'];
+    $sex            = $_POST['sex'];
+    $birth          = $_POST['birth'];
+    $age            = $_POST['age'];
+    $favorite_color = $_POST['favorite_color'];
+
+
+    // connect to the database
+    $mysql_connection = mysqli_connect('mysql.cs.dixie.edu', 'sking', 'P@$$word', 'sking');
+    
+    $sql = "INSERT INTO kids (first_name, middle_name, sex, birth, age, favorite_color) VALUES (
+    '$first_name', 
+    '$middle_name', 
+    '$sex', 
+    '$birth', 
+    '$age', 
+    '$favorite_color'
+    )";
+    
+    mysqli_query($mysql_connection, $sql);
+    
+  } else {
+    // redirect if not POST
+    header('Location: new.php');
+  }
+    // insert into the table
+?>
+
+<div class="row">
+  <div class="card col s6 push-s3">
+    <div class="card-content">
+      <span class="card-title">You added <?= $first_name ?></span>
+      <div class="card-content">
+        <p>
+          <strong>First Name:</strong>
+          <?= $first_name ?>
+        </p>
+        <p>
+          <strong>Middle Name:</strong>
+          <?= $middle_name ?>
+        </p>
+        <p>
+          <strong>Sex:</strong>
+          <?= $sex ?>
+        </p>
+        <p>
+          <strong>Birth Date:</strong>
+          <?= $birth ?>
+        </p>
+        <p>
+          <strong>Age:</strong>
+          <?= $age ?>
+        </p>
+        <p>
+          <strong>Favorite Color:</strong>
+          <?= $favorite_color ?>
+        </p>
+        <div class="card-action">
+          <a class="waves-effect btn blue accent-1" href="index.php">Back to home page</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include('../includes/footer.php'); ?>
---
+++ assignment_4/kids/index.php
@@ -0,0 +1,35 @@
+<?php include('../includes/header.php'); ?>
+
+<?php
+  $mysql_connection = mysqli_connect('mysql.cs.dixie.edu', 'sking', 'P@$$word', 'sking');
+  $sql = "SELECT * FROM kids";
+  $result = mysqli_query($mysql_connection, $sql);
+?>
+
+<h1>Lots of kids</h1>
+<table class="striped">
+  <thead>
+    <tr>
+      <th>First Name</th>
+      <th>Middle Name</th>
+      <th>Sex</th>
+      <th>Birth Date</th>
+      <th>Age</th>
+      <th>Favorite Color</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php while($row = mysqli_fetch_array($result)): ?>
+    <tr>
+      <td><?= $row['first_name'] ?></td>
+      <td><?= $row['middle_name'] ?></td>
+      <td><?= $row['sex'] ?></td>
+      <td><?= $row['birth'] ?></td>
+      <td><?= $row['age'] ?></td>
+      <td><?= $row['favorite_color'] ?></td>
+    </tr>
+    <?php endwhile ?>
+  </tbody>
+</table>
+
+<?php include('../includes/footer.php'); ?>
