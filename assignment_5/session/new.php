b76efdc2bbe0988e2a2f479540abde6dd2f8afd0
Stephen King <stephen@rocketmade.com>
2016-02-18 15:54:16 -0700

    created new and destroy files for session

---
+++ assignment_5/session/new.php
@@ -0,0 +1,31 @@
+<?php include '../includes/header.php' ?>
+
+  <div class="row">
+    <div class="col s6 push-s3">
+      <h1>User Sign In</h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="card col s6 push-s3">
+      <div class="card-content">
+        <span class="card-title">Sign in right now</span>
+        <form method="POST" action="../session/create.php">
+          <div class="input-field">
+            Email:
+            <input type="email" name="email">
+          </div>
+          <div class="input-field">
+            Password:
+            <input type="password" name="password">
+          </div>
+          <div class="card-action">
+            <button class="waves-effect btn teal accent-2" type="submit" name="submit">
+              <span class="pink-text">Sign in</span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+<?php include '../includes/footer.php' ?>
