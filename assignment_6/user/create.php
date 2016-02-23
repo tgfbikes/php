63ff28665a5da88188f8ebf470e8c6ac57b64177
Nick <ld.tech@outlook.com>
2016-02-23 09:37:49 -0700

    Show errors on when sign up fails

--- assignment_6/user/create.php
+++ assignment_6/user/create.php
@@ -1,6 +1,6 @@
 <?php require_once('../includes/header.php'); ?>
 
-<?php 
+<?php
   if (isset($_POST['submit'])) {
     // get values from $_POST
     $first_name       = mysqli_real_escape_string($mysql_connection, $_POST['first_name']);
@@ -8,9 +8,9 @@
     $email            = mysqli_real_escape_string($mysql_connection, $_POST['email']);
     $password         = $_POST['password'];
     $confirm_password = $_POST['confirm_password'];
-    
+
     $errors = [];
-    
+
     if (empty($email)) {
       $errors['email'] = "Please enter your email";
     }
@@ -25,16 +25,16 @@
       $password = '';
       $confirm_password = '';
     }
-    
+
     if (empty($errors)) {
       print_r($errors);
       $encrypted_password = password_hash($password, PASSWORD_DEFAULT);
 
-      // insert into the table
+      insert into the table
       $sql = "INSERT INTO users (first_name, last_name, email, encrypted_password) VALUES (
-      '$first_name', 
-      '$last_name', 
-      '$email', 
+      '$first_name',
+      '$last_name',
+      '$email',
       '$encrypted_password'
       )";
 
@@ -50,36 +50,88 @@
         header('Location: ../user/new.php');
       }
     }
-    
-  } else {
-    // redirect if not POST
-    header('Location: new.php');
+
   }
 ?>
 
-<div class="row">
-  <div class="card col s6 push-s3">
-    <div class="card-content">
-      <span class="card-title"><?= $first_name ?> signed up</span>
+<?php if (empty($errors)): ?>
+
+  <div class="row">
+    <div class="card col s6 push-s3">
       <div class="card-content">
-        <p>
-          <strong>First Name:</strong>
-          <?= $first_name ?>
-        </p>
-        <p>
-          <strong>Last Name:</strong>
-          <?= $last_name ?>
-        </p>
-        <p>
-          <strong>Email:</strong>
-          <?= $email ?>
-        </p>
-        <div class="card-action">
-          <a class="waves-effect btn blue accent-1" href="../kids/index.php">Back to home page</a>
+        <span class="card-title"><?= $first_name ?> signed up</span>
+        <div class="card-content">
+          <p>
+            <strong>First Name:</strong>
+            <?= $first_name ?>
+          </p>
+          <p>
+            <strong>Last Name:</strong>
+            <?= $last_name ?>
+          </p>
+          <p>
+            <strong>Email:</strong>
+            <?= $email ?>
+          </p>
+          <div class="card-action">
+            <a class="waves-effect btn blue accent-1" href="../kids/index.php">Back to home page</a>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
+
+<?php else: ?>
+
+  <!-- I couldn't get the line of code, require_once('new.php') to work so I included all of the code from new.php -->
+  <div class="row">
+    <div class="col s6 push-s3">
+      <h1>User Sign Up</h1>
+      <?php if (isset($errors)): ?>
+        <p>The following errors occurred:</p>
+        <ul>
+        <?php foreach ($errors as $field => $error): ?>
+         <li><?= $error ?></li>
+        <?php endforeach ?>
+        </ul>
+      <?php endif ?>
+    </div>
+  </div>
+  <div class="row">
+    <div class="card col s6 push-s3">
+      <div class="card-content">
+        <span class="card-title">Fill it out stat</span>
+        <form method="post" action="create.php">
+          <div class="input-field">
+            First name:
+            <input type="text" name="first_name" value="<?= strIfSet($first_name) ?>">
+          </div>
+          <div class="input-field">
+            Last name:
+            <input type="text" name="last_name" value="<?= strIfSet($last_name) ?>">
+          </div>
+          <div class="input-field">
+            Email:
+            <input type="email" name="email" value="<?= strIfSet($email) ?>">
+          </div>
+          <div class="input-field">
+            Password:
+            <input type="password" name="password">
+          </div>
+          <div class="input-field">
+            Confirm Password:
+            <input type="password" name="confirm_password">
+          </div>
+          <div class="card-action">
+            <button class="waves-effect btn teal accent-2" type="submit" name="submit">
+              <span class="pink-text">Sign up</span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+<?php endif ?>
 
 <?php require_once('../includes/footer.php'); ?>
