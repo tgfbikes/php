ab2b696fe488bdd5cf64add18e5939c31dda1c04
Stephen King <stephen@rocketmade.com>
2016-02-23 21:13:35 -0700

    configured errors for signin

--- assignment_6/session/create.php
+++ assignment_6/session/create.php
@@ -1,26 +1,47 @@
-<?php include '../includes/header.php' ?>
+<?php require_once('../includes/header.php') ?>
 <?php
   if (isset($_POST['submit'])) {
     // get values from $_POST
-    $email          = $_POST['email'];
+    $email          = mysqli_real_escape_string($mysql_connection, $_POST['email']);
     $password       = $_POST['password'];
     
-    $encrypted_password = password_hash($password, PASSWORD_DEFAULT);
+    $errors = [];
     
-    $sql = "SELECT * FROM users WHERE email = '$email'";
-    $result = mysqli_query($mysql_connection, $sql);
-    
-    if ($row = mysqli_fetch_array($result)) {
-      if (password_verify($password, $row['encrypted_password'])) {
-        $_SESSION['user_id'] = $row['id'];
-        header('Location: ../kids/index.php');
+    if (empty($email)) {
+      $errors['email'] = "Please enter your email";
+    }
+    if (empty($password)) {
+      $errors['password'] = "Please enter your password";
+    }
+                                                
+    if (empty($errors)) {
+      $encrypted_password = password_hash($password, PASSWORD_DEFAULT);
+
+      $sql = "SELECT * FROM users WHERE email = '$email'";
+      $result = mysqli_query($mysql_connection, $sql);
+
+      if ($row = mysqli_fetch_array($result)) {
+        if (password_verify($password, $row['encrypted_password'])) {
+          $_SESSION['user_id'] = $row['id'];
+          header('Location: ../kids/index.php');
+        }
       }
     }
   }
 ?>
 
+<?php if (empty($errors)): ?>
+
   <h2>Something is jacked...</h2>
   <p>Either your email or password is incorrect. Please check both and <a href="new.php">login again</a></p>
   <p>Or if you don't have an accout you can <a href="../user/create.php">sign up</a></p>
 
-<?php include '../includes/footer.php' ?>
+<?php else: ?>
+
+  <?php
+    require_once('new.php');
+  ?>
+  
+<?php endif ?>
+
+<?php require_once('../includes/footer.php') ?>
