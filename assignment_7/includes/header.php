d46070dbe234a9e5ec097f059deb6a2ccbf50360
Stephen King <stephen@rocketmade.com>
2016-02-29 20:35:00 -0700

    deleted kids directory, not needed

--- assignment_7/includes/header.php
+++ assignment_7/includes/header.php
@@ -7,20 +7,19 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Kids</title>
+  <title>Uploads</title>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
 </head>
 <body>
-  <nav class="pink">
+  <nav class="blue">
     <div class="nav-wrapper">
       <div class="row">
         <div class="col s12">
-          <a href="#" class="brand-logo center">I got kids</a>
-          <?php if ($row = getCurrentUser()): ?>
+          <a href="#" class="brand-logo center">Upload a photo</a>
             <ul id="nav-mobile" class="left hide-on-med-and-down">
-              <li><a href="../kids/index.php">Home</a></li>
-              <li><a href="../kids/new.php">New</a></li>
+              <li><a href="../upload/index.php">Home</a></li>
+              <li><a href="../upload/new.php">Upload a new photo</a></li>
             </ul>
             <ul class="right">
               <li><i class="material-icons">perm_identity</i></li>
@@ -31,12 +30,6 @@
                 </form>
               </li>
             </ul>
-          <?php else: ?>
-            <ul class="left hide-on-med-and-down">
-              <li><a href="../user/new.php">Sign Up</a></li>
-              <li><a href="../session/new.php">Sign In</a></li>
-            </ul>
-          <?php endif ?>
         </div>
       </div>
     </div>
