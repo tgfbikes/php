98219d30875e0f8c3b8e043e6c76fef6a70c6b78
Stephen King <stephen@rocketmade.com>
2016-03-02 11:40:52 -0700

    included container

--- slimapi/index.php
+++ slimapi/index.php
@@ -5,9 +5,32 @@
   // Create and configure Slim app
   $app = new \Slim\App;
 
-  // Define app routes
-  $app->get('/hello/{name}', function ($request, $response, $args) {
-      return $response->write("Hello " . $args['name']);
+  // Get container
+  $container = $app->getContainer();
+
+  // Register component on container
+  $container['view'] = function ($container) {
+      return new \Slim\Views\PhpRenderer('templates');
+  };
+
+  // Index action
+  $app->get('/beards', function ($request, $response) {
+    return $response->write('index');
+  });
+
+  // New action
+  $app->get('/beards/new', function ($request, $response) {
+    return $response->write('new');
+  });
+
+  // Show action
+  $app->get('/beards/{id}', function ($request, $response) {
+    return $response->write('show');
+  });
+
+  // Create action
+  $app->post('/beards', function ($request, $response) {
+    return $response->write('create');
   });
 
   // Run app
