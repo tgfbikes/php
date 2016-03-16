b85a92c89c80dfb5534a1dcb4e443eefcd2d4e56
Stephen King <stephen@rocketmade.com>
2016-03-16 09:27:08 -0600

    created index and show

---
+++ slimapi/templates/index.html.php
@@ -0,0 +1,31 @@
+<?php require('./includes/header.php'); ?>
+
+<div class="row">
+  <div class="col s6 push-s3">
+    <h1>Beards</h1>
+  </div>
+</div>
+<div class="row">
+  <div class="card col s6 push-s3">
+    <div class="card-content">
+      <span class="card-title">Beards</span>
+      <form action="create.php" method="POST" enctype="multipart/form-data">
+        <div class="input-field">
+          Title:
+          <input type="text" name="title" value="">
+        </div>
+        <div class="input-field">
+          File:
+          <input type="file" name="file">
+        </div>
+        <div class="card-action">
+          <button class="waves-effect btn teal accent-2" type="submit" name="submit">
+            <span class="pink-text">Upload Photo</span>
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+            
+<?php require_once('./includes/footer.php'); ?>
---
+++ slimapi/templates/show.html.php
@@ -0,0 +1,9 @@
+This is the show page
+
+<?php if ($beard): ?>
+
+  <?= $beard['beard']; ?>
+
+<?php else: ?>
+  <p>Nothing to show</p>
+<?php endif ?>
