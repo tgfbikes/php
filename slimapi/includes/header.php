dde7432816af8b17e8f146a84760453632f54fa7
Stephen King <stephen@rocketmade.com>
2016-03-17 09:39:05 -0600

    changed some of the static strings

--- slimapi/includes/header.php
+++ slimapi/includes/header.php
@@ -1,13 +1,11 @@
 <?php
   session_start();
-//  require_once('mysql.php');
-//  require('functions.php');
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Uploads</title>
+  <title>Beards of Plenty</title>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
 </head>
@@ -16,10 +14,10 @@
     <div class="nav-wrapper">
       <div class="row">
         <div class="col s12">
-          <a href="#" class="brand-logo center">Upload a photo</a>
+          <a href="#" class="brand-logo center">Beards of Plenty</a>
             <ul id="nav-mobile" class="left hide-on-med-and-down">
-              <li><a href="../upload/index.php">Home</a></li>
-              <li><a href="../upload/new.php">Upload a new photo</a></li>
+              <li><a href="./beards">Home</a></li>
+              <li><a href="./beards/new">Upload a new beard</a></li>
             </ul>
         </div>
       </div>
