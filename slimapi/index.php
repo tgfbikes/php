4b7985d0d2b4069cb4d59b3b2cbc9b304672da04
Stephen King <stephen@rocketmade.com>
2016-03-17 09:39:27 -0600

    uncommented index route, mysql error

--- slimapi/index.php
+++ slimapi/index.php
@@ -1,7 +1,15 @@
 <?php
 
-  require('vendor/autoload.php');
-  require('includes/Beard.php');
+  require('./vendor/autoload.php');
+  require('./includes/Beard.php');
+
+  function renderJSON($response, $status_code, $data) {
+    $json = json_encode($data);
+    return $response
+      ->write($json)
+      ->withStatus($status_code)
+      ->withHeader('Content-Type', 'application/json');
+  }
   
   // Create and configure Slim app
   $app = new \Slim\App;
@@ -17,10 +25,18 @@
     return new \Slim\Views\PhpRenderer('templates/');
   };
 
+  // Index action .json
+  $app->get('/beards.json', function ($request, $response) {
+    $beardDB = new Beard();
+    $beards = $beardDB->getBeards();
+    $json = json_encode($beards);
+    return renderJSON($response, 200, $beards);
+  });
+
   // Index action
   $app->get('/beards', function ($request, $response) {
-//    $beardDB = new Beard();
-//    $beards = $beardDB->getBeards();
+    $beardDB = new Beard();
+    $beards = $beardDB->getBeards();
     return $this->view->render($response, 'index.html.php', [
       'beards' => $beards
     ]);
@@ -29,6 +45,14 @@
   // New action
   $app->get('/beards/new', function ($request, $response) {
     return $this->view->render($response, 'new.html.php');
+  });
+
+  // Show action .json
+  $app->get('/beards/{id}.json', function ($request, $response, $args) {
+    $id = $args['id'];
+    $beardDB = new Beard();
+    $beard = $beardDB->getBeard($id);
+    return renderJSON($response, 200, $beard);
   });
 
   // Show action
@@ -41,12 +65,20 @@
     ]);
   });
 
+  // Create action .json
+  $app->post('/beards.json', function ($request, $response) {
+    $body = $request->getParsedBody(); // $_POST associative array
+    $beardDb = new Beard();
+    $beardDb->createBeard($body);
+    return renderJSON($response, 201, $beard);
+  });
+
   // Create action
   $app->post('/beards', function ($request, $response) {
     $body = $request->getParsedBody(); // $_POST associative array
     $beardDb = new Beard();
     $beardDb->createBeard($body);
-    return $response->withStatus(301)->withHeader('Location', '/slimapi/index.php/beards');
+    return $response->withStatus(302)->withHeader('Location', '/slimapi/index.php/beards');
   });
 
   // Run app
